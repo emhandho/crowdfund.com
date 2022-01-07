b288c99950f739b71a62785a5d561161ae971d89
emhandho <nurmanrdh@gmail.com>
2022-01-07 15:00:24 +0700

    replace env variables name and resetting docker-compose file

--- main.go
+++ main.go
@@ -30,11 +30,11 @@ func connection() (*gorm.DB, error) {
 		log.Printf("Unable to load .env file %v", err)
 	}
 
-	db_user := os.Getenv("MYSQL_USER")
-	db_password := os.Getenv("MYSQL_PASSWORD")
-	db_host := os.Getenv("MYSQL_HOST")
-	db_port := os.Getenv("MYSQL_PORT")
-	db_name := os.Getenv("MYSQL_DATABASE")
+	db_user := os.Getenv("DB_USER")
+	db_password := os.Getenv("DB_PASSWORD")
+	db_host := os.Getenv("DB_HOST")
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_DATABASE")
 
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", db_user, db_password, db_host, db_port, db_name)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
